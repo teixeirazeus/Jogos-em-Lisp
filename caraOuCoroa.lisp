18e376db7b369b45c32fa2b996e5f9d095591755
teixeira.zeus <teixeira.zeus@gmail.com>
2021-10-11 16:23:42 -0300

    cara ou coroa

---
+++ caraOuCoroa.lisp
@@ -0,0 +1,25 @@
+(defun printNreturn (value)
+    (format t value)
+    value
+)
+
+(defun get-number ()
+    (let ((number (random 2 (make-random-state t))))
+        (if (= number 0)
+        (printNreturn "Cara!")
+        (printNreturn "Coroa!"))))
+
+(defun prompt ()
+    (format t "Digite cara ou coroa: ")
+    (defvar chute (string-downcase (read)))
+    (if (equal chute "cara")
+        "Cara!"
+        (if (equal chute "coroa")
+            "Coroa!"))
+)
+
+(defun game ()
+    (if (string= (prompt) (get-number))
+    (format t "Você ganhou!")
+    (format t "Você perdeu!")))
+(game)
